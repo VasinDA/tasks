20d9ffbe23e4b54b5536746d51c95d4cf946db41
VasinDA <svga.shop@gmail.com>
2022-06-21 20:14:57 +0300

    converter

--- task_3/arabic_to_roman_convertor.py
+++ task_3/arabic_to_roman_convertor.py
@@ -1,11 +1,19 @@
 class Convertor:
     def __init__(self):
        self.units_dict = {1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 
-       6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX'}
-       self.tens_dict ={10: 'X', 20: 'XX', 30: 'XXX'}
+       6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX', }
+       self.tens_dict = {1: 'X', 2: 'XX', 3: 'XXX'}
 
-    def convertRomansToArabic(self, num):
+    def convertArabicToRomans(self, num):
         if not isinstance(num, int):
             raise 'Not digit'
         if num == 0:
             return ''
+        units = num % 10
+        tens = num // 10
+        if tens == 0:
+            return self.units_dict[units]
+        if units == 0:
+            return self.tens_dict[tens]
+        return '{}{}'.format(self.tens_dict[tens], self.units_dict[units])
+
