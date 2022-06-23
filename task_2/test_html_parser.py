0c28e64f6217670ab6f0bc6a773201811789a861
Denis Vasin <svga.shop@gmail.com>
2022-06-23 13:18:20 +0300

    fix path to test files

--- task_2/test_html_parser.py
+++ task_2/test_html_parser.py
@@ -4,7 +4,8 @@ from html_parser import HtmlParser
 class TestHtmlParser(unittest.TestCase):
     def test_get_text_from_html(self):
         parser = HtmlParser()
-        test_dictionary = {'test_files\index1.html':'123', 'test_files\index2.html':'test', 'test_files\index3.html':'Text', 'test_files\index4.html':'Here is the test', 'test_files\index5.html':'My test is here'}
+        test_dictionary = {'test_files\\index1.html':'123', 'test_files\\index2.html':'test', 'test_files\\index3.html':'Text', 'test_files\\index4.html':'Here is the test', 
+        'test_files\\index5.html':'My test is here'}
         for key in test_dictionary:
             self.assertCountEqual(parser.getTextFromFile(key), test_dictionary[key])
 
