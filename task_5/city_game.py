469b215ce7fdb9fafe7e70361e69ac8ac7495263
VasinDA <svga.shop@gmail.com>
2022-06-26 18:27:41 +0300

    city_game

--- task_5/city_game.py
+++ task_5/city_game.py
@@ -2,14 +2,42 @@ from codecs import utf_8_encode
 
 
 class CityGame:
-    def readCitiesFromFile(self, file_name):
+    def __init__(self, file_name) :
+        self.alfabet_str = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ'
         try:
             with open(file_name, 'r', encoding='utf-8') as file:
                 cities_list = file.readlines()
-            return cities_list
+            self.cities_dict = {letter: [city.strip('\n') for city in cities_list if city.startswith(letter)] 
+            for letter in self.alfabet_str if len([city.strip('\n') for city in cities_list if city.startswith(letter)]) > 0}
         except IOError:
             raise IOError("Reading file error")
 
-    def getCityFromList(letter):
+    def getCityByLetter(self, letter):
+        if not letter.isalpha() and len(letter) > 1:
+            return 'Not letter'
         letter = letter.upper()
+        if letter not in self.cities_dict:
+            return 'No cities starting with this letter'
+        for key in self.cities_dict:
+            if key == letter:
+                if len(self.cities_dict[key]) == 0:
+                    return 'Ran out of cities starting with this letter'
+                answer_dict = {}
+                for i in range(len(self.cities_dict[key]) - 1):
+                    city = self.cities_dict[key][i]
+                    last_letter = city[len(city) - 1].upper()
+                    if last_letter in self.cities_dict:
+                        if len(self.cities_dict[last_letter]) not in answer_dict:
+                            answer_dict[len(self.cities_dict[last_letter])] = i            
+                    answer = self.cities_dict[key][answer_dict[min(answer_dict)]]
+        new_cities_list = self.cities_dict[letter]
+        del new_cities_list[answer_dict[min(answer_dict)]]
+        self.cities_dict[letter] = new_cities_list
+        return answer
         
+
+a = CityGame('cities_list.txt')
+print(a.getCityByLetter('Я'))
+
+                    
+
