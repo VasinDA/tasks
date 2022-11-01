5485c840632406f96efc19e2a952f060e16f4052
Denis Vasin <svga.shop@gmail.com>
2022-11-01 20:38:56 +0200

    task_12 8min

---
+++ task_12/not_duplicate.py
@@ -0,0 +1,9 @@
+def notDuplicate(list):
+    dict = {}
+    for i in list:
+        if i not in dict:
+            dict[i] = 0
+        dict[i] +=1
+    print(dict)
+    list = [key for key in dict if dict[key] == 1]
+    return list
