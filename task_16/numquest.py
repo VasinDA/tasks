1b0ff7f7837cfc55a519b6faaa6187226b820bbc
Denis Vasin <svga.shop@gmail.com>
2022-11-07 16:08:13 +0200

    numquest

--- task_16/numquest.py
+++ task_16/numquest.py
@@ -1,2 +1,13 @@
+from math import log
+
 def numQuest(higt_limit):
-    if not isinstance()
+    upper_limit = int(input('Enter upper limit'))
+    flag = True
+    list_for_quest = [i for i in range(, upper_limit + 1)]
+    low = 0
+    high = upper_limit - 1
+    mid = (high + low) // 2
+    try_count = log(upper_limit)
+    counter = 0
+    while flag == True:
+                
