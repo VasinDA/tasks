3aab942d44e88d7918c2f62eb65d7737811fb405
Denis Vasin <svga.shop@gmail.com>
2022-11-07 20:44:29 +0200

    numquest

--- task_16/numquest.py
+++ task_16/numquest.py
@@ -1,13 +1,29 @@
-from math import log
+from math import log, ceil
 
-def numQuest(higt_limit):
-    upper_limit = int(input('Enter upper limit'))
-    flag = True
-    list_for_quest = [i for i in range(, upper_limit + 1)]
+def numQuest():
+    upper_limit = int(input('Enter upper limit: '))
+    list_for_quest = [i for i in range(1, upper_limit + 1)]
     low = 0
     high = upper_limit - 1
-    mid = (high + low) // 2
-    try_count = log(upper_limit)
+    try_count = ceil(log(upper_limit))
     counter = 0
-    while flag == True:
-                
+    print(f'I will guess in {try_count} attempts')
+    return searchForSolutionTheQuest(list_for_quest, low, high, counter)
+
+def searchForSolutionTheQuest(list_for_quest, low, high, counter):
+    if high >= low:
+        counter += 1
+        mid = (high + low) // 2
+        print(list_for_quest[mid])
+        question = input('If the number is less than the specified - "less" \nIf the number is greater than the specified - "more" \nIf guessed - "yes" \n')
+        if question.lower() == 'yes':
+            return counter
+        elif question.lower() == 'less':
+            return searchForSolutionTheQuest(list_for_quest, low, mid - 1, counter)
+        elif question.lower() == 'more':
+            return searchForSolutionTheQuest(list_for_quest, mid + 1, high, counter)
+        else:
+            return searchForSolutionTheQuest(list_for_quest, low, high, counter)
+
+
+print(numQuest())
