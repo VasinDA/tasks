7195ee9558925d25841d33c2f6f60ea2d1977ddf
Denis Vasin <svga.shop@gmail.com>
2022-11-08 18:48:10 +0200

    task_17

--- task_17/delduplicate.py
+++ task_17/delduplicate.py
@@ -4,8 +4,7 @@ def delDuplicate(mylist):
     for i in mylist:
         if i not in mydict:
             newlist.append(i)
+            print(newlist)
         mydict[i] = 0
-    return mylist
+    return newlist
         
-mylist = [1,2,2,4,3,2,1,5,6]
-print(delDuplicate(mylist))
