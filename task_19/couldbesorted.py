2583eee98cafb51696486e943d7b83fa49e89e58
Eugene Lagoda <eugene.lagoda@gmail.com>
2022-11-12 00:26:59 -0800

    TODOs

--- task_19/couldbesorted.py
+++ task_19/couldbesorted.py
@@ -1,10 +1,13 @@
+# TODO: Coulde we resolve it for O(n)?
 def couldBeSorted(mylist):
     counter = 0
     len_list = len(mylist)
+    # TODO: the condition is wrong, let's check with [6,2,3,4,5,1]
     if min(mylist) == mylist[0] or max(mylist) == mylist[len_list - 1]:
         return beSortedUp(mylist, counter, len_list)
     return beSortedDown(mylist, counter, len_list)
 
+# TODO: let's think about parameters, do we really need of them?
 def beSortedUp(mylist, counter, len_list):
     for i in range(len_list):
         lowest_index = i
@@ -17,6 +20,7 @@ def beSortedUp(mylist, counter, len_list):
         mylist[i], mylist[lowest_index] = mylist[lowest_index], mylist[i]
     return True
 
+# TODO: could we have only 1 function for sorting? Maybe with param `direction`...
 def beSortedDown(mylist, counter, len_list):
     for i in range(len_list):
         biggest_index = i
