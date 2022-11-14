5b34af1ae0bb9c7bd6db1d98b6284ce17b3d3856
Denis Vasin <svga.shop@gmail.com>
2022-11-14 18:03:03 +0200

    task_19

--- task_19/couldbesorted.py
+++ task_19/couldbesorted.py
@@ -1,30 +1,19 @@
-def couldBeSorted(mylist):
-    counter = 0
-    len_list = len(mylist)
-    if min(mylist) == mylist[0] or max(mylist) == mylist[len_list - 1]:
-        return beSortedUp(mylist, counter, len_list)
-    return beSortedDown(mylist, counter, len_list)
+#def couldBeSorted(mylist):
 
-def beSortedUp(mylist, counter, len_list):
-    for i in range(len_list):
-        lowest_index = i
-        if counter > 1:
-                return False
-        for j in range(i + 1, len_list):
-           if mylist[j] < mylist[lowest_index]:
-                lowest_index = j
-                counter += 1
-        mylist[i], mylist[lowest_index] = mylist[lowest_index], mylist[i]
-    return True
 
-def beSortedDown(mylist, counter, len_list):
-    for i in range(len_list):
-        biggest_index = i
-        if counter > 1:
-                return False
-        for j in range(i + 1, len_list):
-           if mylist[j] > mylist[biggest_index]:
-                biggest_index = j
-                counter += 1
-        mylist[i], mylist[biggest_index] = mylist[biggest_index], mylist[i]
-    return True
+
+def swapCounter(mylist):
+    swap_dict = {'>':0, '<':0, 'swap_up':[], 'swap_down':[]}
+    for index in range(len(mylist)-1):
+        value = mylist[index]
+        next_value = mylist[index + 1]
+        if value < next_value:
+            swap_dict['swap_up'].append(index)
+            swap_dict['>'] += 1
+        if value > next_value:
+            swap_dict['swap_down'].append(index + 1)
+            swap_dict['<'] += 1
+    return swap_dict 
+
+mylist = [6,2,3,4,5,1]
+print(swapCounter(mylist))
---
+++ task_19/couldbesorted1.py
@@ -0,0 +1,30 @@
+def couldBeSorted(mylist):
+    counter = 0
+    len_list = len(mylist)
+    if min(mylist) == mylist[0] or max(mylist) == mylist[len_list - 1]:
+        return beSortedUp(mylist, counter, len_list)
+    return beSortedDown(mylist, counter, len_list)
+
+def beSortedUp(mylist, counter, len_list):
+    for i in range(len_list):
+        lowest_index = i
+        if counter > 1:
+                return False
+        for j in range(i + 1, len_list):
+           if mylist[j] < mylist[lowest_index]:
+                lowest_index = j
+                counter += 1
+        mylist[i], mylist[lowest_index] = mylist[lowest_index], mylist[i]
+    return True
+
+def beSortedDown(mylist, counter, len_list):
+    for i in range(len_list):
+        biggest_index = i
+        if counter > 1:
+                return False
+        for j in range(i + 1, len_list):
+           if mylist[j] > mylist[biggest_index]:
+                biggest_index = j
+                counter += 1
+        mylist[i], mylist[biggest_index] = mylist[biggest_index], mylist[i]
+    return True
