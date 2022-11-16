2cd67531ad1b02ffa86126f7761f17db234f5e0c
Denis Vasin <svga.shop@gmail.com>
2022-11-16 15:12:08 +0200

    task_21

---
+++ task_21/multiplicate.py
@@ -0,0 +1,32 @@
+#solution 1
+def multiplicationSol1(a,b):
+    c = abs(a)
+    d = abs(b)
+    multi = c
+    for _ in range(d-1):
+        multi += c
+    if a < 0 or b < 0:
+        return -multi
+    return multi
+
+#solution 2
+def multiplicationSol2(a, b):
+    c = abs(a)
+    d = abs(b)
+    mylist = [c for _ in range(d)]
+    if a< 0 or b < 0:
+        return -sum(mylist)
+    return sum(mylist)   
+
+#solution 3
+def multiplicationSol3(a, b):
+    c = abs(a)
+    d = abs(b)
+    mylist = []
+    if a < 0 or b < 0:
+        return -sum(recursion(c, d, mylist))
+    return sum(recursion(c, d, mylist))
+
+def recursion(c, d, mylist):
+    mylist.append(c)
+    return recursion((c, d - 1, mylist))
