3494e465286dfcb004d9e04b757500142b36aefd
Denis Vasin <svga.shop@gmail.com>
2022-12-06 21:33:50 +0200

    task_25

--- task_25/palindrome.py
+++ task_25/palindrome.py
@@ -3,22 +3,20 @@ def palinDrome(str):
         return False
     if str.isdigit():
         return False
-    if len(str) == 1:
+    len_str = len(str)
+    if len_str == 0:
         return True
-    str_list = list(str)
-    len_str_list_half = len(str_list) // 2
-    counter =  len_str_list_half
-    for idx in range(len_str_list_half):
-        if str_list[idx].isdigit():
-            # TODO: may we eleminate the `del` amd ise L- and R- pointers?
-            del str_list[idx]
-            palinDrome(''.join(str_list))
-        if str_list[-idx - 1].isdigit():
-            del str_list[-idx - 1]
-            palinDrome(''.join(str_list))
-        if str_list[idx].lower() == str_list[-idx - 1].lower():
-            counter -= 1
-    return counter == 0
-
+    start = 0 
+    end = len_str - 1
+    while start < end:
+        while str[start].isdigit():
+            start += 1
+        while str[end].isdigit():
+            end -= 1
+        if str[start].lower() != str[end].lower():
+            return False
+        start += 1
+        end -= 1
+    return True
 
 
--- task_25/palindrome_test.py
+++ task_25/palindrome_test.py
@@ -4,8 +4,9 @@ from palindrome import palinDrome
 class TestPalinDrome(unittest.TestCase):
     def test_palindrome(self):
         # TODO: to check with "133a123" -> True
-        test_dict = {'арозаупаланалапуазора': True, 'арозаупаланалапуазорА': True, 'Ка1бак': True, 'a': True, '': False,
-        '1111111': False, 'a124a': True, '4': False}
+        test_dict = {'арозаупаланалапуазора': True, 'арозаупаланалапуазорА': True, 
+        'Ка1бак': True, 'a': True, '': False,'1111111': False, 'a124a': True, 
+        '4': False, '133a123': True}
         for key in test_dict:
             self.assertEqual(palinDrome(key), test_dict[key])
 
