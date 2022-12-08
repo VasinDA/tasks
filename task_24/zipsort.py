092ee46276e9b5817efc19d667a512cddac5a4f7
Denis Vasin <svga.shop@gmail.com>
2022-12-08 20:06:42 +0200

    task_24

--- task_24/zipsort.py
+++ task_24/zipsort.py
@@ -1,15 +1,19 @@
 def zipSort(arr1, arr2):
     if not arr1 and not arr2:
         return []
+    arr1_len = len(arr1)
+    arr2_len = len(arr2)
+    index_arr1 = 0
+    index_arr2 = 0
     new_arr = []
-    while arr1 and arr2:
-        if arr1[0] < arr2[0]:
-            new_arr.append(arr1[0])
-            del arr1[0]
-        elif arr1[0] > arr2[0]:
-            new_arr.append(arr2[0])
-            del arr2[0]
-    if arr1:
-        new_arr = new_arr + arr1
-    new_arr = new_arr + arr2
+    while index_arr1 < arr1_len and index_arr2 < arr2_len:
+        while arr1[index_arr1] < arr2[index_arr2]:
+            new_arr.append[arr1[index_arr1]]
+            index_arr1 += 1
+        while arr2[index_arr2] < arr1[index_arr1]:
+            new_arr.append[arr2[index_arr2]]
+            index_arr2 += 1
+    if index_arr1 < arr1_len:
+        new_arr = new_arr + arr1[index_arr1:]
+    new_arr = new_arr + arr2[index_arr2:]
     return new_arr
