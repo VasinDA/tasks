e1f84e7be71f32cce25cb9da61168c6a1275451a
Denis Vasin <svga.shop@gmail.com>
2022-12-08 20:14:39 +0200

    task_24

--- task_24/zipsort_test.py
+++ task_24/zipsort_test.py
@@ -2,15 +2,22 @@ import unittest
 from zipsort import zipSort
 
 class ZipSortTest(unittest.TestCase):
-    def zipsort_test(self):
-        test_arr1 = [1,2,4]
-        test_arr2 = [3,5]
+    def setUp(self):
+        self.test_arr1 = [1,2,4]
+        self.test_arr2 = [3,5]
+        self.test_arr3 = []
+        
+    def zipsort_test_1(self):
         epxpected_result_1 = [1,2,3,4,5]
-        test_arr3 = []
+        self.assertEqual(zipSort(self.test_arr1, self.test_arr2), epxpected_result_1)
+        
+    def zipsort_test_2(self):
         epxpected_result_2 = [3,5]
+        self.assertEqual(zipSort(self.test_arr2, self.test_arr3), epxpected_result_2)
+        
+    def zipsort_test_3(self):
         epxpected_result_3 = []
-        self.assertEqual(zipSort(test_arr1, test_arr2), epxpected_result_1)
-        self.assertEqual(zipSort(test_arr2, test_arr3), epxpected_result_2)
-        self.assertEqual(zipSort(test_arr3, test_arr3), epxpected_result_3)
+        self.assertEqual(zipSort(self.test_arr3, self.test_arr3), epxpected_result_3)
+
 
 unittest.main()
