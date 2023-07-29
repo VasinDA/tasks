0552de74f9634bd37bef3d4cf9dbf938854e271c
Denis Vasin <svga.shop@gmail.com>
2023-07-29 15:40:40 +0200

    fixed

--- task_15/sumoftwo.py
+++ task_15/sumoftwo.py
@@ -2,8 +2,7 @@ def sumOfTwo(mylist, sum):
     mydict = {}
     for index in range(len(mylist)):
         value = sum - mylist[index]
-        if value in mydict:
-            return [mydict[value], index]
+        if mylist[index] in mydict:
+            return [mydict[mylist[index]], index]
         mydict[value] = index
-
-
+        
